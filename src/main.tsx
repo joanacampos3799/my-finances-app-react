acc6c23ad4ad7632a10d2e2daa324083f4a84717
Joana Campos <joana.campos.786@gmail.com>
2024-08-28 16:31:09 +0100

    Added Authentication

---
+++ src/main.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+// Import the layouts
+import RootLayout from "./Layouts/RootLayout";
+import DashboardLayout from "./Layouts/DashboardLayout";
+
+// Import the components
+import IndexPage from "./Pages/index";
+import SignInPage from "./Pages/SignIn";
+import SignUpPage from "./Pages/SignUp";
+import DashboardPage from "./Pages/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <IndexPage /> },
+      { path: "/sign-in/*", element: <SignInPage /> },
+      { path: "/sign-up/*", element: <SignUpPage /> },
+      {
+        element: <DashboardLayout />,
+        path: "dashboard",
+        children: [{ path: "/dashboard", element: <DashboardPage /> }],
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
